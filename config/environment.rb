ed04cb381594bb47047185bae9a7819fe54cc4c4
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-11-12 16:19:27 -0600

    Prepared files for heroku deployment

--- config/environment.rb
+++ config/environment.rb
@@ -35,10 +35,12 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
 
-env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
+if development?
+	env_config = YAML.load_file(APP_ROOT.join('config', 'twitter.yaml'))
 
-env_config.each do |key, value|
-  ENV[key] = value
+	env_config.each do |key, value|
+	  ENV[key] = value
+	end
 end
 
 Twitter.configure do |config|
